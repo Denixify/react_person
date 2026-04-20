31794841fd68270e42151416458a57582584b259
Denys Boiko <big4ever6996@gmail.com>
2026-04-20 18:05:57 +0200

    problem_fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,8 @@
 export const Person = ({ person }) => {
+  // eslint-disable-next-line prettier/prettier
   const { name, age, sex, isMarried, partnerName } = person;
 
+  // eslint-disable-next-line prettier/prettier
   const partnerRole = sex === 'm' ? 'wife' : 'husband';
 
   const partnerText = isMarried
